13a91b6dd5414eaa236b7de5e3228d7d97b72c1e
Jacky <1206290545@qq.com>
2021-03-07 21:50:20 +0800

    * d_lock

--- src/main/java/com/example/demo/controller/StockController.java
+++ src/main/java/com/example/demo/controller/StockController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/v0/stocks")
 @RequiredArgsConstructor
@@ -16,11 +18,24 @@ public class StockController {
 
 	private final static String PRODUCT_NAME = "phone";
 
+	/**
+	 * 分布式锁的名称
+	 */
+	private final static String LOCK_NAME = "d_lock";
+
 	@GetMapping("/deduction")
 	public String deduction() {
+		// lock id
+		String uuid = UUID.randomUUID().toString();
 		// 1. 获取 redis 锁
-		synchronized (this) {
-			Integer stock = Integer.valueOf(stringRedisTemplate.opsForValue().get(PRODUCT_NAME));
+		Boolean lockState = stringRedisTemplate.opsForValue().setIfAbsent(LOCK_NAME, uuid);
+		if (!lockState) {
+			return "手速太快，服务器跟不上了...";
+		}
+
+		// 业务逻辑
+		try {
+			int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get(PRODUCT_NAME));
 			if (stock > 0) {
 				Integer newStock = stock - 1;
 				stringRedisTemplate.opsForValue().set(PRODUCT_NAME, newStock.toString());
@@ -29,7 +44,11 @@ public class StockController {
 				System.out.println("扣减库存失败，剩余库存不足～");
 			}
 		}
+
 		// 2.  释放锁
+		finally {
+			stringRedisTemplate.delete(LOCK_NAME);
+		}
 		return "end";
 	}
 

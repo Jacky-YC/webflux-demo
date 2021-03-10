f1f3f509bdd9d9f48445f1a627c39571907b6a58
Jacky <1206290545@qq.com>
2021-03-10 21:36:14 +0800

    * redisson distribution lock

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,39 @@
 package com.example.demo;
 
+import lombok.RequiredArgsConstructor;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
 @EnableR2dbcRepositories
+@RequiredArgsConstructor
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+	private final Environment env;
+
+	@Bean
+	public RedissonClient redissonClient() {
+		Config config = new Config();
+
+		String host = env.getProperty("spring.redis.host");
+		String port = env.getProperty("spring.redis.port");
+		String address = "redis://" + host + ":" + port;
+
+		System.out.println(address);
+		config.useSingleServer()
+				.setAddress(address);
+		return Redisson.create(config);
 	}
 
 }
--- src/main/java/com/example/demo/controller/StockController.java
+++ src/main/java/com/example/demo/controller/StockController.java
@@ -1,18 +1,20 @@
 package com.example.demo.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v0/stocks")
 @RequiredArgsConstructor
 public class StockController {
 
+
+	private final RedissonClient redisson;
 
 	private final StringRedisTemplate stringRedisTemplate;
 
@@ -25,29 +27,19 @@ public class StockController {
 
 	@GetMapping("/deduction")
 	public String deduction() {
-		// lock id
-		String uuid = UUID.randomUUID().toString();
-		// 1. 获取 redis 锁
-		Boolean lockState = stringRedisTemplate.opsForValue().setIfAbsent(LOCK_NAME, uuid);
-		if (!lockState) {
-			return "手速太快，服务器跟不上了...";
-		}
-
-		// 业务逻辑
+		RLock lock = redisson.getLock(LOCK_NAME);
 		try {
+			lock.lock();
 			int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get(PRODUCT_NAME));
 			if (stock > 0) {
 				Integer newStock = stock - 1;
 				stringRedisTemplate.opsForValue().set(PRODUCT_NAME, newStock.toString());
-				System.out.println("扣减库存成功，剩余库存: " + newStock);
+				System.out.println("扣减库存成功，剩余库存: " + stock);
 			} else {
 				System.out.println("扣减库存失败，剩余库存不足～");
 			}
-		}
-
-		// 2.  释放锁
-		finally {
-			stringRedisTemplate.delete(LOCK_NAME);
+		} finally {
+			lock.unlock();
 		}
 		return "end";
 	}
